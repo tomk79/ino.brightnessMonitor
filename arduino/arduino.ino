434298ca16ef6261e5186de730c656988297afd4
Tomoya Koyanagi <tomk79@gmail.com>
2013-10-17 01:27:52 +0900

    Cdsに連動するようになった。
    - 配線イメージの写真を追加。
    - 一旦動かしてみた動画も追加。

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,9 @@
 /**
  * brightness monitor
  */
-int count = 0;
+int potpin0 = 0;
+int val0;
+int val0last;
 
 void setup(){
   Serial.begin(9600);
@@ -10,11 +12,14 @@ void setup(){
 }
 
 void loop(){
-  Serial.print(count);
-  Serial.print("\n");
 
-  count ++;
-  if( count >= 256 ){ count = 0; }
+  val0 = analogRead(potpin0);//可変抵抗器からの入力
+  val0 = map(val0, 0, 1023, 0, 255);//可変抵抗は0〜1023の範囲で値を返す。解像度を合わせる。
 
+  if( val0last != val0 ){
+    Serial.print(val0);
+    Serial.print("\n");
+    val0last = val0;
+  }
   delay(100);
 }
